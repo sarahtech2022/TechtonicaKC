b7226d4ec0f86c4be2ca18e4d9cbc9569a058829
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-24 10:46:14 -0700

    saving my work

--- Week6KC/server/index.js
+++ Week6KC/server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
-app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
+app.use(express.static("../client"));
+express.static(path.join(_dirnam,"/client");)
 
 app.get("/", (req, res) => {
   res.json("Hello, this will be a single page");
 });
+
+app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
