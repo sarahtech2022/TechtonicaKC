7b565e9bf6060e72ad641d4fa9d45b8d91965f3f
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-24 10:52:06 -0700

    fixed the error

--- Week6KC/server/index.js
+++ Week6KC/server/index.js
@@ -8,10 +8,11 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.static("../client"));
-express.static(path.join(_dirnam,"/client");)
+//express.static(path.join(_dirnam,"/client");)
 
 app.get("/", (req, res) => {
-  res.json("Hello, this will be a single page");
+  //res.json("Hello, this will be a single page");
+  res.sendFile(path.join(_dirname, "../client/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
