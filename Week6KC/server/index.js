c59f747eafd2bb2e38bb93d29e971c9b79b695b6
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-24 11:14:52 -0700

    fixed my underscores in dirname

--- Week6KC/server/index.js
+++ Week6KC/server/index.js
@@ -7,12 +7,19 @@ const app = express();
 const PORT = 8080;
 
 app.use(cors());
+const jsonParser = bodyParser.json();
+
 app.use(express.static("../client"));
 //express.static(path.join(_dirnam,"/client");)
 
 app.get("/", (req, res) => {
   //res.json("Hello, this will be a single page");
-  res.sendFile(path.join(_dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+app.get("/aboutme", (req, res) => {
+  //res.json("Hello, this will be a single page");
+  res.sendFile(path.join(__dirname, "../client/aboutme.html"));
 });
 
 app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
